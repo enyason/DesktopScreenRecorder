44a039aeacedcd9c1c65a346bc0aa7add1d17187
enyason <enyasonjnr@yahoo.com>
2018-08-16 00:08:27 +0100

    Implemented screen recording task

---
+++ src/desktopscreenrecorder/ScreenRecorderTask.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desktopscreenrecorder;
+
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+import org.jcodec.api.awt.AWTSequenceEncoder;
+
+/**
+ *
+ * @author enyason
+ */
+public class ScreenRecorderTask extends TimerTask {
+
+    AWTSequenceEncoder encoder;
+
+    Robot robot;
+    Rectangle screenDimension;
+    BufferedImage image;
+    
+ 
+   
+  
+    public ScreenRecorderTask(AWTSequenceEncoder sequenceEncoder, Rectangle rectangle) {
+        encoder = sequenceEncoder;
+        screenDimension = rectangle;
+       
+        try {
+            robot = new Robot();
+        } catch (AWTException ex) {
+
+            Logger.getLogger(ScreenRecorderTask.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      
+        RecordTimer.start();
+
+
+    }
+
+    @Override
+    public void run() {
+               
+        System.out.println("encoiding...");
+        
+        image = robot.createScreenCapture(screenDimension);
+       
+//       if(MainScreenRecorderFrame.timerRecord != null){
+       
+       try {
+            encoder.encodeImage(image);
+        } catch (IOException ex) {
+            Logger.getLogger(ScreenRecorderTask.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+//       }
+
+
+    }
+
+}
