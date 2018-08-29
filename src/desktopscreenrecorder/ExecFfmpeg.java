642b05c7bb5b1c2e09edca9c18cc9b04463db57b
enyason <enyasonjnr@yahoo.com>
2018-08-29 18:44:47 +0100

    Implemented ffmeg to mux aduio and video files

--- src/desktopscreenrecorder/ExecFfmpeg.java
+++ src/desktopscreenrecorder/ExecFfmpeg.java
@@ -17,35 +17,14 @@ import java.util.logging.Logger;
  */
 public class ExecFfmpeg {
 
-    public static void main(String[] args) {
-        
-       convertToAvi();
-
-    }
-
-    public void muxVideoAndAudio() {
-
-        String excec = "ffmpeg -i convertedJava.avi -i audio.wav -c:v copy -c:a aac -strict experimental outputFromJava.mp4";
-
-    }
-
-    public static void convertToAvi() {
+    public static void executeFfMpeg(String exec) {
 
 //     String exec = "ffmpeg -i video_ouput.mp4 -q:v 0 convertedJava.avi";
-        String exec = "ffmpeg -i convertedJava.avi -i audio.wav -c:v copy -c:a aac -strict experimental outputFromJava.mp4";
-
+//        String exec = "ffmpeg -i convertedJava.avi -i audio.wav -c:v copy -c:a aac -strict experimental outputFromJava.mp4";
         Runtime runtime = Runtime.getRuntime();
 
         try {
             Process process = runtime.exec(exec);
-
-            BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String line = null;
-
-            while ((line = input.readLine()) != null) {
-                System.out.println(line);
-            }
 
             int exitVal = process.waitFor();
             System.out.println("Exited with error code " + exitVal);
